9d82a7dc0391dc0655199c266b3f52f246ba0f19
rahulmua <rahulmua@LAPTOP-K0BMQIS7>
2023-03-10 22:04:36 +0530

    print message modified first time

--- src/demo/pack1/PrintMsg.java
+++ src/demo/pack1/PrintMsg.java
@@ -5,6 +5,9 @@ public class PrintMsg {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 
 	}
 
